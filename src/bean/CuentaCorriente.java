6332fafdd1a51a5a7c156774830715b406c16f2a
fok09 <botiukfranco@gmail.com>
2018-10-29 18:27:07 -0300

    Modificaciones a CuentaCorriente

--- src/bean/CuentaCorriente.java
+++ src/bean/CuentaCorriente.java
@@ -1,21 +1,50 @@
 package bean;
+
 import java.util.*;
+
 public class CuentaCorriente {
-	
+
 	private float saldo;
 	private Vector<Factura> facturas;
 	private Cliente cliente;
+
+	public CuentaCorriente() {
+
+	}
+	
+	public CuentaCorriente(float saldo, Vector<Factura> facturas, Cliente cliente) {
+		super();
+		this.saldo = saldo;
+		this.facturas = facturas;
+		this.cliente = cliente;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
 	
 	public float calcularSaldo(String cuit_cuil) {
-			
-		float saldo=0;
-		for(Factura f: this.facturas) {
+
+		float saldo = 0;
+		for (Factura f : this.facturas) {
 			if (f.getCliente().getCuit_cuil().equals(cuit_cuil)) {
-				if(f.isPagado()==false)
+				if (f.isPagado() == false)
 					saldo = saldo + f.getMonto();
 			}
 		}
-		
+
 		return saldo;
 	}
 }
