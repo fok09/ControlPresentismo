0a54041e5a5778e738701b089e4da72c354f3833
fok09 <botiukfranco@gmail.com>
2018-10-29 12:44:20 -0300

    Sincro V1

--- src/bean/Fichada.java
+++ src/bean/Fichada.java
@@ -1,5 +1,24 @@
 package bean;
 
+import java.sql.Time;
+import java.util.*;
+
 public class Fichada {
 
+	private Time horaEntrada;
+	private Time horaSalida;
+	
+	private Empleado empleado;
+	
+	
+	public void getHoraEntrada()
+	{
+		this.horaEntrada.getTime();
+	}
+	
+	public void getHoraSalida()
+	{
+		this.horaSalida.getTime();
+	}
+	
 }
