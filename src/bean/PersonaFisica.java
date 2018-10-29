51142645f6039ca16f993bf7be4d2900dd4a28cb
KarT <KarT@DESKTOP-L5QE6FQ>
2018-10-29 15:45:19 -0300

    Personas

---
+++ src/bean/PersonaFisica.java
@@ -0,0 +1,19 @@
+package bean;
+
+public class PersonaFisica extends Cliente{
+	private String nombre;
+	private String apellido;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+}
---
+++ src/bean/PersonaJuridica.java
@@ -0,0 +1,14 @@
+package bean;
+
+public class PersonaJuridica extends Cliente{
+	private String razonSocial;
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	
+}
