1ed395917cef32990b94a5102bd988d3518f1d8d
fok09 <botiukfranco@gmail.com>
2018-10-31 18:57:54 -0300

    Agregado de interfaz remota

---
+++ src/interfaces/CargaEmpleado.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
+import bean.Contratacion;
+import dto.ContratacionDTO;
+import dto.ClienteDTO;
+import dto.EmpleadoDTO;
+import dto.FacturaDTO;
+import dto.FichadaDTO;
+import dto.PersonaFisicaDTO;
+import dto.PersonaJuridicaDTO;
+
+public interface CargaEmpleado extends Remote {
+
+	//metodos con DTOs
+	public void cargaEmpleado(String CUITEmpresa, String legajo,String nombre, String apellido, String mail, String dni, String telefono) throws RemoteException;
+
+}
