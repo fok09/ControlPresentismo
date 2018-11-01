eab08661f4e7a503fdfc0301fba24fbbd81f430f
SMACTR <SMACTR@SMAP00000165.baires.techint.net>
2018-11-01 16:46:02 -0300

    ertert

--- src/hbt/testHibernate.java
+++ src/hbt/testHibernate.java
@@ -1,27 +1,16 @@
 package hbt;
 
-import java.sql.Time;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import bean.Cliente;
-import bean.Empleado;
-import bean.Fichada;
-import bean.PersonaFisica;
 import bean.Servicio;
-import srv.ClienteSrv;
 import srv.ServicioSrv;
 
 public class testHibernate {
 
 	public static void main(String[] args) {
 		HibernateUtil.getSessionFactory();
-		
-		Servicio s1 = new Servicio ("Mensual",250, (float) 0.02);
-		Servicio s2 = new Servicio ("Eventual",200, (float) 0.02);
-		Servicio s3 = new Servicio ("Semanal",100, (float) 0.02);
+
+		Servicio s1 = new Servicio("Mensual", 250, (float) 0.02);
+		Servicio s2 = new Servicio("Eventual", 200, (float) 0.02);
+		Servicio s3 = new Servicio("Semanal", 100, (float) 0.02);
 		ServicioSrv.grabarServicio(s1);
 		ServicioSrv.grabarServicio(s2);
 		ServicioSrv.grabarServicio(s3);
